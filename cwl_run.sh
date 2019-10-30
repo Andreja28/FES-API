337be87ea5a5b1a0a371fc0f9c01c0c3c6bedc48
Andreja28 <andrejazivic2@gmail.com>
2019-10-30 16:49:02 +0100

    Minor changes

--- cwl_run.sh
+++ cwl_run.sh
@@ -1,4 +1,5 @@
 # subprocess.Popen(['cwltoil','--jobStore',job_store_path, cwl_path, yaml_path])
 cd $4
-#echo cwltoil --jobStore $3 $1 $2
+echo "$4"
+echo "cwltoil --jobStore $3 $1 $2"
 cwltoil --jobStore "$3" "$1" "$2"
