ff5f387880635b91e0b4561cda827c481117fa97
Andreja28 <andrejazivic2@gmail.com>
2019-10-30 16:28:13 +0100

    Minor changes

--- hello.py
+++ hello.py
@@ -40,7 +40,8 @@ def run_workflow():
         print(yaml_path)
         print(os.path.abspath(job_store_path))
         print(os.path.abspath(out_dir))
-        subprocess.Popen(['bash cwl_run.sh', cwl_path, yaml_path, os.path.abspath(job_store_path), os.path.abspath(out_dir)])
+        subprocess.Popen(['bash cwl_run.sh', cwl_path, yaml_path, os.path.abspath(job_store_path), 
+        os.path.abspath(out_dir)], stderr="file.out")
     elif (req_data['type'] == 'toil'):
         toil_path = os.path.join(config.TOIL, 'main.py')
 
