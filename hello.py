dd8edcba77d032a26e8a004e4edf1627fc13f76f
Andreja28 <andrejazivic2@gmail.com>
2019-10-31 15:25:17 +0100

    Added timeout

--- hello.py
+++ hello.py
@@ -40,12 +40,12 @@ def run_workflow():
         print(yaml_path)
         print(os.path.abspath(job_store_path))
         print(os.path.abspath(out_dir))
-        subprocess.Popen('timeout '+req_data['timeout']+' bash cwl_run.sh ' + cwl_path+" "+ yaml_path+" "+ os.path.abspath(job_store_path)+" "+
+        subprocess.Popen('timeout -k '+req_data['timeout']+' bash cwl_run.sh ' + cwl_path+" "+ yaml_path+" "+ os.path.abspath(job_store_path)+" "+
         os.path.abspath(out_dir), shell=True)
     elif (req_data['type'] == 'toil'):
         toil_path = os.path.join(config.TOIL, 'main.py')
 
-        subprocess.Popen(['timeout',req_data['timeout'],'python', config.TOIL +req_data["workflow"]+"/main.py", job_store_path,out_dir])
+        subprocess.Popen(['timeout -k',req_data['timeout'],'python', config.TOIL +req_data["workflow"]+"/main.py", job_store_path,out_dir])
     else:
         return {'status':'FAILED' }
 
